6446a8eabbb47c4c4e1889d13329179a2da39ffb
BohdanTarchanin <32395057+BohdanTarchanin@users.noreply.github.com>
2024-03-17 16:43:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,22 @@ st.header('Please upload an image')
 
 # upload file
 file = st.file_uploader('', type=['png', 'jpg', 'jpeg'])
+
+checkFile = ("model.pth")
+    for path in checkFile:
+        if os.path.exists(path) == False:
+            print('I miss :', path)
+            msg = st.warning("🚩 Models need to be downloaded... ")
+            try:
+                with st.spinner('Initiating...'):
+                    time.sleep(3)
+                    url_pth = "https://drive.google.com/file/d/1XTevverAgBxlZXRzpRdzR9gYM4YvoKgA/uc?export=download"
+
+                    r_pth = requests.get(url_pth, allow_redirects=True)
+
+                    open("wandering-sponge-4.pth", 'wb').write(r_pth.content)
+                    open("label_embeddings.npy", 'wb').write(r_npy.content)
+                    del r_pth
+                    msg.success("Download was successful ✅")
+            except:
+                msg.error("Error downloading model files...😥")
