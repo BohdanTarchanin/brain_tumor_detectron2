7e8503e48699cf2b75720560b47018425b8d2244
BohdanTarchanin <32395057+BohdanTarchanin@users.noreply.github.com>
2024-03-17 21:11:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
-from detectron2 import model_zoo
+#from detectron2.config import get_cfg
+#from detectron2.engine import DefaultPredictor
+#from detectron2 import model_zoo
 from PIL import Image
 import numpy as np
 import requests
@@ -22,44 +22,3 @@ st.header('Будь ласка, завантажте зображення')
 # upload file
 file = st.file_uploader('', type=['png', 'jpg', 'jpeg'])
 
-url = "https://drive.google.com/uc?id=1XTevverAgBxlZXRzpRdzR9gYM4YvoKgA"
-output = "model.pth"
-gdown.download(url, output)
-
-# load model
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/retinanet_R_101_FPN_3x.yaml'))
-cfg.MODEL.WEIGHTS = 'model.pth'
-cfg.MODEL.DEVICE = 'cpu'
-
-predictor = DefaultPredictor(cfg)
-
-# load image
-if file:
-    image = Image.open(file).convert('RGB')
-
-    image_array = np.asarray(image)
-
-    # detect objects
-    outputs = predictor(image_array)
-
-    threshold = 0.5
-
-    # Display predictions
-    preds = outputs["instances"].pred_classes.tolist()
-    scores = outputs["instances"].scores.tolist()
-    bboxes = outputs["instances"].pred_boxes
-
-    bboxes_ = []
-    for j, bbox in enumerate(bboxes):
-        bbox = bbox.tolist()
-
-        score = scores[j]
-        pred = preds[j]
-
-        if score > threshold:
-            x1, y1, x2, y2 = [int(i) for i in bbox]
-            bboxes_.append([x1, y1, x2, y2])
-
-    # visualize
-    visualize(image, bboxes_)
